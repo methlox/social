1794ab57f265dea041fbfc1c60652054f9ccb032
methlox <astroyellowh@gmail.com>
2024-02-23 23:41:00 +0530

    all the constants

--- app/src/utils/constants.ts
+++ app/src/utils/constants.ts
@@ -1,4 +1,9 @@
-import { UserSidebarItemType, ConversationTypeData } from "./types";
+import {
+  UserSidebarItemType,
+  ConversationTypeData,
+  ContextMenuItemType,
+  SettingsItemType,
+} from "./types";
 
 export const userSidebarItems: UserSidebarItemType[] = [
   {
@@ -25,31 +30,102 @@ export const userSidebarItems: UserSidebarItemType[] = [
 
 export const chatTypes: ConversationTypeData[] = [
   {
-    type: 'private',
-    label: 'Private',
+    type: "private",
+    label: "Private",
   },
   {
-    type: 'group',
-    label: 'Group',
+    type: "group",
+    label: "Group",
   },
 ];
 
 export enum CDN_URL {
-  BASE = 'https://chuachat.ams3.cdn.digitaloceanspaces.com/',
-  ORIGINAL = 'https://chuachat.ams3.cdn.digitaloceanspaces.com/original/',
-  PREVIEW = 'https://chuachat.ams3.digitaloceanspaces.com/preview/',
+  BASE = "https://chuachat.ams3.cdn.digitaloceanspaces.com/",
+  ORIGINAL = "https://chuachat.ams3.cdn.digitaloceanspaces.com/original/",
+  PREVIEW = "https://chuachat.ams3.digitaloceanspaces.com/preview/",
 }
 
 export enum WebsocketEvents {
-  VOICE_CALL_ACCEPTED = 'onVoiceCallAccepted',
-  VOICE_CALL_HANG_UP = 'onVoiceCallHangUp',
-  VOICE_CALL_REJECTED = 'onVoiceCallRejected',
-  VIDEO_CALL_REJECTED = 'onVideoCallRejected',
+  VOICE_CALL_ACCEPTED = "onVoiceCallAccepted",
+  VOICE_CALL_HANG_UP = "onVoiceCallHangUp",
+  VOICE_CALL_REJECTED = "onVoiceCallRejected",
+  VIDEO_CALL_REJECTED = "onVideoCallRejected",
 }
 
 export enum SenderEvents {
-  VIDEO_CALL_INITIATE = 'onVideoCallInitiate', 
-  VIDEO_CALL_ACCEPT = 'videoCallAccepted',
-  VOICE_CALL_INITIATE = 'onVoiceCallInitiate',
-  VOICE_CALL_ACCEPT = 'onVoiceCallAccepted',
+  VIDEO_CALL_INITIATE = "onVideoCallInitiate",
+  VIDEO_CALL_ACCEPT = "videoCallAccepted",
+  VOICE_CALL_INITIATE = "onVoiceCallInitiate",
+  VOICE_CALL_ACCEPT = "onVoiceCallAccepted",
+}
+
+export const userContextMenuItems: ContextMenuItemType[] = [
+  {
+    label: "Kick User",
+    action: "kick",
+    color: "#ff0000",
+    ownerOnly: true,
+  },
+  {
+    label: "Transfer Owner",
+    action: "transfer_owner",
+    color: "#FFB800",
+    ownerOnly: true,
+  },
+  {
+    label: "Profile",
+    action: "profile",
+    color: "#7c7c7c",
+    ownerOnly: false,
+  },
+];
+
+export const friendsNavbarItems = [
+  {
+    id: "friends",
+    label: "Friends",
+    pathname: "/friends",
+  },
+  {
+    id: "requests",
+    label: "Requests",
+    pathname: "/friends/requests",
+  },
+  {
+    id: "blocked",
+    label: "Blocked",
+    pathname: "/friends/blocked",
+  },
+];
+
+export const settingsItems: SettingsItemType[] = [
+  {
+    id: "profile",
+    label: "Profile",
+    pathname: "/settings/profile",
+  },
+  {
+    id: "security",
+    label: "Security",
+    pathname: "/settings/security",
+  },
+  {
+    id: "notifications",
+    label: "Notifications",
+    pathname: "/settings/notifications",
+  },
+  {
+    id: "integrations",
+    label: "Integrations",
+    pathname: "/settings/integrations",
+  },
+  {
+    id: "appearance",
+    label: "Appearance",
+    pathname: "/settings/appearance",
+  },
+];
+
+export enum ReceiverEvents {
+  VOICE_CALL = "onVoiceCall",
 }
