9eb23411a7835be7310dc36c2ec61725ce17f655
methlox <astroyellowh@gmail.com>
2024-02-23 14:32:54 +0530

    routes for conversations, groups and calls

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { PropsWithChildren, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route element={<AuthenticatedRoute children={<AppPage />} />}>
+          <Route path="conversations" element={<ConversationPage />}>
+            <Route
+              path=":id"
+              element={
+                <ConversationPageGuard children={<ConversationChannelPage />} />
+              }
+            />
+          </Route>
+          <Route path="groups" element={<GroupPage />}>
+            <Route
+              path=":id"
+              element={<GroupPageGuard children={<GroupChannelPage />} />}
+            />
+          </Route>
+          <Route path="friends" element={<FriendsLayoutPage />}>
+            <Route path="requests" element={<FriendRequestPage />} />
+            <Route path="blocked" element={<div>Blocked</div>} />
+          </Route>
+          <Route path="settings" element={<SettingsPage />}>
+            <Route path="profile" element={<SettingsProfilePage />} />
+            <Route path="appearance" element={<SettingsAppearancePage />} />
+          </Route>
+          <Route path="calls" element={<CallsPage />}>
+            <Route path="current" element={<CurrentCallPage />} />
+          </Route>
+        </Route>
+      </Routes>
   );
 }
 
